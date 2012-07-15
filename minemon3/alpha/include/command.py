c4cf0bfe856d4b7013ad1929c07707528bf0803d
Oscar <oscar@eth1.nu>
2012-07-15 13:02:31 +0200

    Alpha 9
    Last alpha of MM3, application has reached same functionality as MM2

---
+++ minemon3/alpha/include/command.py
@@ -0,0 +1,270 @@
+import action
+import random
+import os
+import filecmp
+import sys
+import time
+
+#### Actual commands ####
+
+def sheen():
+    sheenstuff = ["Winning", "Bi-winning", "Win Win everywhere", "Your face will melt off!", "Whats not to love?", "Epic Winning!", "Win here, win there, win everywhere!", "Absolute victory!", "That's how I roll", "No pants? - Winning!", "Duuh, WINNING! WINNING!", "The only thing Im addicted to right now is winning."]
+    win = random.randint(0, 11)
+    bro = (sheenstuff[win])
+    action.say(bro, 0)
+
+def login(chatlog):
+
+    #get nick
+    name = chatlog
+    name = name[27:]
+    bort = ' '
+    name = name.split(bort, 1)[0]
+
+    #Choose greeting
+    hello = ["Welcome ", "Greetings ", "Hai ", "Hai thar ", "Oh hai ", "Hello "]
+    greetnum = random.randint(0,5)
+    hail = (hello[greetnum])
+
+    #check who's online
+    online = action.send("list", 0.1)
+    online = online.split("Connected players: ")[-1]
+    online = online.split(name, 1)[0] #remove playername from online
+    if online == "":
+        online = "None. "
+    online = online[:-2]
+
+    #greet and return nick
+    action.say(hail + name + "! Online: " + online, 0)
+    return name
+
+def logout(chatlog):
+
+    #get nick
+    name = chatlog
+    name = name[27:]
+    bort = ' '
+    name = name.split(bort, 1)[0]
+
+    #say goodbye & return nick for logg
+    action.say("Goodbye " + name + " !", 0)
+    return name
+
+def hax(name):
+    action.send("gamemode " + name + " 1", 0)
+
+def unhax(name):
+    action.send("gamemode " + name + " 0", 0)
+
+def day():
+    action.send("time set 10", 0)
+
+def night():
+    action.send("time set 14000", 0)
+
+def tp(name, chatlog):
+    who = chatlog
+    who = who[28:]
+    who = who.split("> !tp")[-1]
+    who = who.replace("\n", "")
+    if who == "":
+        action.say("Need a target!", 0)
+        return " (No target) "
+    else:
+        who = who.split(" ")[-1]
+        action.send("tp " + name +" "+ who, 0)
+        return who
+
+def pull(name, chatlog):
+    who = chatlog
+    who = who[28:]
+    who = who.split("> !tp")[-1]
+    who = who.replace("\n", "")
+    if who == "":
+        action.say("Need a target!", 0)
+        return " (No target) "
+    else:
+        who = who.split(" ")[-1]
+        action.send("tp " + who +" "+ name, 0)
+        return who
+
+def map(url):
+    action.say("See full map at: "+url, 0)
+
+def help(url):
+    action.say("See full commandlist at: "+url, 0)
+
+def roll(name):
+    roll = random.randint(1, 100)
+    roll = str(roll)
+    if roll == "100":
+        action.say(name + " rolled "+roll+" and gets rewarded with a diamond!", 0.5)
+        action.send("give " + name + " 57 1", 0)
+    else:
+        action.say(name + " rolled "+roll, 0)
+    return roll
+
+def rain():
+    action.send("toggledownfall", 0)
+    action.say("Loading ...", 1.5)
+    action.say("rain/snow was turned on/off", 0)
+
+def xp(name):
+    for antal in range(0,4):
+        action.send("xp " + name + " 5000", 0.2)
+
+def kit(name):
+    item = 276
+    for antal in range(0,3):
+        item = item + 1
+        action.send("give " + name +" "+ str(item) + " 1", 0.1)
+
+def leatherset(name):
+    item = 297
+    for antal in range(0,4):
+        item = item + 1
+        action.send("give " + name +" "+ str(item) + " 1", 0.1)
+    action.send("give " + name + " 268 1", 0)
+
+def diamondset(name):
+    item = 309
+    for antal in range(0,4):
+        item = item + 1
+        action.send("give " + name +" "+ str(item) + " 1", 0.1)
+    action.send("give " + name + " 276 1", 0)
+
+def bow(name):
+    action.send("give " + name + " 261 1", 0)
+    for antal in range(0,3):
+        action.send("give " + name + " 262 64", 0.1)
+
+def train(name):
+    action.send("give " + name + " 328 5", 0.1)
+    action.send("tell " + name + " Do _NOT_ leave empty trains on the rail!", 0)
+
+def sleep(name):
+    action.send("give " + name + " 355 5 ", 0)
+
+def rail(name):
+    for antal in range(0,4):
+        action.send("give " + name + " 27 64", 0.1)
+    for antal in range(0,2):
+        action.send("give " + name + " 28 64", 0.1)
+
+def food(name):
+    for antal in range(0,5):
+        action.send("give " + name + " 363 64", 0.1)
+
+def item(name, chatlog):
+    item = chatlog
+    item = item[28:]
+    item = item.split("!item ")[-1]
+    item = item.replace("\n", "")
+    action.send("give " + name +" "+ item +" 64", 0)
+    return item
+
+def restart():
+    #stop
+    action.say("[Warning] Server restarting in 10 seconds!", 5)
+    action.say("[Warning] Server restarting in 5 seconds!", 5)
+    action.send("save-all", 2)
+    action.stop_server()
+
+    #start
+    #yeah this is hard though rcon >.<
+    action.start_server()
+
+def mail(name, chatlog):
+        issue = chatlog
+        issue = issue[28:]
+        issue = issue.split("> !report")[-1]
+        issue = issue.replace("\n", "")
+        issue = "[ "+name+" ] "+issue
+        action.mail(issue)
+        action.say("Problem was reported to the administrator!", 0)
+
+def monsters(path):
+    #real and tempfile
+    properties = path + "server.properties"
+    mc_out = "/tmp/tempsett.txt"
+
+    #open them
+    mc_settings = open(properties, "r")
+    mc_out = open(mc_out, "w")
+
+    # loop through the test file line by line
+    # and write only the lines back out that do
+    # not contain the search string
+    search = "spawn-monsters="
+    for line in mc_settings:
+        if search not in line:
+            mc_out.write(line)
+        else:
+            print "found monsters line!"
+            print line
+            if "false" in line:
+                action.say("[Warning] Turning monsters ON!", 2)
+                action.say("[Warning] Monsters will appear in the next night cycle.", 3)
+                action.stop_server()
+                mc_out.write("spawn-monsters=True\n")
+                return "ON"
+            if "true" in line:
+                action.say("[Warning] Turning monsters OFF!", 2)
+                action.say("[Warning] Monsters will stop spawning.", 3)
+                action.stop_server()
+                mc_out.write("spawn-monsters=False\n")
+                return "OFF"
+            action.send_sys("rm " + properties, 0)
+            action.send_sys("mv /tmp/tempsett.txt " + properties, 0)
+            time.sleep(1)
+            action.start_server()
+
+    # close the file handles
+    mc_settings.close()
+    mc_out.close()
+
+def update(path, port):
+    action.say("Downloading minecraft_server...", 0)
+
+    #remove any old leftovers
+    action.send_sys("rm /tmp/"+port+"/minecraft_server.jar", 1)
+
+    #download latest version
+    action.send_sys("wget -b --directory-prefix=/tmp/"+port+"/ https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar", 10)
+    action.say("Done, comparing versions..", 0.5)
+    server = path + "minecraft_server.jar"
+
+    #compare with current
+    compare = filecmp.cmp("/tmp/"+port+"/minecraft_server.jar", server)
+    print "comparing " + '/tmp/'+port+'/minecraft_server.jar' + " with " + server
+
+    #if no difference do nothing
+    if compare == True:
+        action.say("Latest version installed", 1)
+        action.say("- Doing nothing.", 1)
+        return "No update found"
+
+    #else shutdown and update
+    else:
+        action.say("Versions does not match!", 3)
+        action.say("Im going to try something crazy and update myself!", 4)
+        action.say("Dont be mad if shit goes bad :3", 2)
+        action.stop_server()
+        action.send_sys("rm "+ server, 1)
+        action.send_sys("mv /tmp/"+port+"/minecraft_server.jar "+ server, 1)
+        action.start_server()
+        return "Updated server!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ minemon3/alpha/minemon.py
@@ -0,0 +1,320 @@
+# -*- coding: utf-8 -*-
+
+# importing regular stuff
+import os
+import sys
+import thread
+import threading
+import time
+import datetime
+from datetime import datetime
+import random
+import filecmp
+import ConfigParser
+import socket
+
+#my stuff will go here
+import include.action as action
+import include.logreader as logreader
+import include.command as command
+import include.logger as log
+
+#### code start ####
+legit = True
+
+#### version ####
+version = "3.0.0 alpha 8"
+version = str(version)
+print "starting up MineMon "+version
+time.sleep(0.2)
+print "Author: Oscar Carlberg"
+
+#### Load settings ####
+setting_file = sys.argv[1]
+config = ConfigParser.RawConfigParser()
+config.read(setting_file)
+
+#### Connect to MC rcon ###
+mchost = config.get('config', 'rhost')
+mcport = config.get('config', 'rport')
+mcpwd = config.get('config', 'rpass')
+
+#### some settings-var ####
+mcpath = config.get('config', 'mcpath')
+mapurl = config.get('config', 'mapurl')
+helpurl = config.get('config', 'helpurl')
+screen = config.get('config', 'screen')
+
+gmail = config.get('config', 'gmail')
+gpw = config.get('config', 'gmailpw')
+mailrcvr = config.get('config', 'sendto')
+
+#### announce that i'm running ####
+action.connect(mchost, mcport, mcpwd)
+action.load(gmail, gpw, mailrcvr, screen)
+action.say("Minecraft Monitor Version "+version+" now running!", 1)
+action.say("Type !help for available commands", 0)
+
+ops = action.load_op(mcpath)
+
+#### check if enabled & op func ####
+
+def enabled(onoroff):
+    try:
+        setting = config.get('config', onoroff)
+    except:
+        setting = "disabled"
+        print "NO setting entry for "+onoroff+", disabled."
+    if "enabled" in setting:
+        return True
+    else:
+        action.say("This command has been disabled!", 0)
+        return False
+
+def check_op(name):
+    if name.lower() in ops:
+        return True
+    else:
+        action.say("This command is not allowed for non-op's.", 0)
+
+#### Trigger on chattlog stuff ####
+def trigger(name):
+    if "!sheen" in chatlog:
+        if enabled("!sheen"):
+            command.sheen()
+            log.save(timestamp, "TEXT", "!sheen", name)
+
+    elif "logged in with entity" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("login_manner"):
+            player = command.login(chatlog)
+            log.save(timestamp, "GREEN", "Login:", player)
+
+    elif "lost connection:" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("logout_manner"):
+            player = command.logout(chatlog)
+            log.save(timestamp, "RED", "Logout:", player)
+
+    elif "!hax" in chatlog and not "[Rcon]" in chatlog:
+        if enabled("!hax"):
+            if check_op(name):
+                command.hax(name)
+                log.save(timestamp, "SYSTEM", "!hax", name)
+
+    elif "!unhax" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!unhax"):
+            if check_op(name):
+                command.unhax(name)
+                log.save(timestamp, "SYSTEM", "!unhax", name)
+
+    elif "!day" in chatlog:
+        if enabled("!day"):
+            command.day()
+            log.save(timestamp, "SYSTEM", "!day", name)
+
+    elif "!night" in chatlog:
+        if enabled("!night"):
+            command.night()
+            log.save(timestamp, "SYSTEM", "!night", name)
+
+    elif "!tp" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!tp"):
+            who = command.tp(name, chatlog)
+            log.save2(timestamp, "TEXT", "!tp", name, "] [ -> ] [", who)
+
+    elif "!pull" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!pull"):
+            if check_op(name):
+                who = command.pull(name, chatlog)
+                log.save2(timestamp, "TEXT", "!pull", name, "] [ <- ] [", who)
+
+    elif "!map" in chatlog:
+        if enabled("!map"):
+            command.map(mapurl)
+            log.save(timestamp, "SYSTEM", "!map", name)
+
+    elif "!help" in chatlog:
+        if enabled("!help"):
+            command.help(helpurl)
+            log.save(timestamp, "SYSTEM", "!help", name)
+
+    elif "!version" in chatlog:
+        action.say("Running MineMon version: " + version, 0)
+        log.save(timestamp, "SYSTEM", "!version", name)
+
+    elif "!list" in chatlog:
+        action.say("Deprecated. Press <tab>", 0)
+
+    elif "!roll" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!roll"):
+            roll = command.roll(name)
+            log.save2(timestamp, "TEXT", "!roll", name, "] [", roll)
+
+    elif "!rain" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!rain"):
+            command.rain()
+            log.save(timestamp, "SYSTEM", "!rain", name)
+
+    elif "!xp" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!xp"):
+            if check_op(name):
+                command.xp(name)
+                log.save(timestamp, "TEXT", "!xp", name)
+
+    elif "!kit" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!kit"):
+            command.kit(name)
+            log.save(timestamp, "TEXT", "!kit", name)
+
+    elif "!leatherset" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!leatherset"):
+            command.leatherset(name)
+            log.save(timestamp, "TEXT", "!leatherset", name)
+
+    elif "!diamondset" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!diamondset"):
+            if check_op(name):
+                command.diamondset(name)
+                log.save(timestamp, "TEXT", "!diamondset", name)
+
+    elif "!bow" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!bow"):
+            command.bow(name)
+            log.save(timestamp, "TEXT", "!bow", name)
+
+    elif "!train" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!train"):
+            command.train(name)
+            log.save(timestamp, "TEXT", "!train", name)
+
+    elif "!sleep" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!sleep"):
+            command.sleep(name)
+            log.save(timestamp, "TEXT", "!sleep", name)
+
+    elif "!rail" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!rail"):
+            command.rail(name)
+            log.save(timestamp, "TEXT", "!rail", name)
+
+    elif "!food" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!food"):
+            command.food(name)
+            log.save(timestamp, "TEXT", "!food", name)
+
+    elif "!item" in chatlog and not "[Rcon]" in chatlog:
+        if enabled("!item"):
+            if check_op(name):
+                item = command.item(name, chatlog)
+                log.save2(timestamp, "TEXT", "!item", name, "] [", item)
+
+    elif "!restart" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!restart"):
+            if check_op(name):
+                command.restart()
+                log.save(timestamp, "SYSTEM", "!restart", name)
+
+    elif "!monsters" in chatlog:
+        if enabled("!monsters"):
+            if check_op(name):
+                onoff = command.monsters(mcpath)
+                log.save2(timestamp, "SYSTEM", "!monsters", name, "] [", onoff)
+
+    elif "!update" in chatlog:
+        if enabled("!update"):
+            if check_op(name) or "CONSOLE" in chatlog:
+                status = command.update(mcpath, mcport)
+                log.save2(timestamp, "SYSTEM", "!update", name, "] [", status)
+
+    elif "!temphax" in chatlog and not "CONSOLE" in chatlog:
+        action.say("not implemented yet gogo MYSQL", 0)
+
+    elif "!report" in chatlog and not "CONSOLE" in chatlog:
+        if enabled("!report"):
+            command.mail(name, chatlog)
+            log.save(timestamp, "SYSTEM", "!report", name)
+
+
+
+    elif "Opping" in chatlog or "De-opping" in chatlog:
+        global ops
+        ops = action.load_op(mcpath)
+        action.say("Detecting change in OP's, refreshing list!", 0)
+        log.save(timestamp, "SYSTEM", "OP-refresh", "SYSTEM")
+
+    elif "[INFO] Done (" in chatlog or "[INFO] RCON running on" in chatlog:
+        print "< STARTING SERVER > - Reconnecting to rcon"
+        action.connect(mchost, mcport, mcpwd)
+
+    elif "[INFO] Saving chunks" in chatlog:
+        print "< STOPPING SERVER >"
+
+
+    #old non-supported commands
+    elif "!tnt" in chatlog or "!stone" in chatlog or "!wood" in chatlog or "!dirt" in chatlog:
+        action.say("Deprecated command. use !hax or !item", 0)
+
+
+
+#### Name extractor
+def extract_name(player):
+    # extrahera namn
+    player = player[28:]
+    bort = '>'
+    player = player.split(bort, 1)[0]
+    return player
+
+#### Mainloop ####
+def func_checkLastLine(lastLine):
+    global chatlog
+    global timestamp
+    chatlog = lastLine.replace("\n", "")
+    timestamp = datetime.now()
+    name = extract_name(lastLine)
+    trigger(name)
+
+#### start of S3rR1 hax, i dont even what is this ####
+class newLoopingThread (threading.Thread):
+    def __init__(self, threadID):
+        self.threadID = threadID
+        threading.Thread.__init__(self)
+    def run(self):
+        func_loop()
+
+def func_loop():
+    tempList = fileList
+    while running:
+        time.sleep(0.5)
+        fileHandle = open(logfile, 'r')
+        newLines = fileHandle.readlines()
+        if newLines != tempList and tempList != None:
+            tempList = newLines
+            newList = [item for item in tempList if item != '\n']
+            if len(newList) > 0: func_checkLastLine(newList[len(newList) - 1])
+
+def func_getLastLine():
+    fileHandle = open(logfile, 'r')
+    allLines = fileHandle.readlines()
+    allLines = [item for item in allLines if item != '\n']
+    return allLines[len(allLines) - 1]
+
+#### Start application
+running = True
+logfile = mcpath + "server.log"
+
+fileHandle = open(logfile, 'r')
+fileList = fileHandle.readlines()
+
+loopThread = newLoopingThread(1)
+loopThread.start()
+
+
+#### exit ####
+print "press any key to exit"
+raw_input()
+running = False
+print "Waiting for looping thread to stop..."
+while loopThread.isAlive(): time.sleep(0.5)
+action.say("Minecraft Monitor Version "+version+" stopped!", 0)
+
+#lol = action.send("list")
+#print lol
